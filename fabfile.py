d95bb7eaec8b0c5ae205ddc197e1f4d1d726ee40
jaron <jaron@localhost.localdomain>
2015-02-09 16:26:30 +0800

    add install service_identity

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,10 @@
 # encoding: utf-8
 
-from fabric.api import local, cd, run, env, prefix
+from fabric.api import *
 
-env.hosts=['root@elephant']
+env.hosts=['root@elephant', 'root@vm2']
 
-def install_pythonbrew():
+def install_pythonbrew(version='2.7.7'):
     run('yum install gcc patch zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel python-setuptools -y')
     run('yum groupinstall "Development tools" -y')
     run('easy_install pythonbrew')
@@ -13,22 +13,24 @@ def install_pythonbrew():
         run('curl -O \
             https://gist.githubusercontent.com/jaron92/1692e8836f1311a5e03f/raw/c2a61f9304d70664034dec6739de3731140cd6a4/config.cfg')
     with prefix('source "$HOME/.pythonbrew/etc/bashrc"'):
-        run('pythonbrew uninstall 2.7.7')
-        run('pythonbrew install 2.7.7')
+        run('pythonbrew uninstall %s' % version)
+        run('pythonbrew install %s' % version)
 
 
-def create_venv(venv='ENV'):
+def create_venv(venv='ENV', version='2.7.7'):
     with prefix('source "$HOME/.pythonbrew/etc/bashrc"'):
         run('pythonbrew venv init')
-        run('pythonbrew venv create %s -p 2.7.7' % venv)
+        run('pythonbrew venv create %s -p %s' % (venv, version))
         run('pythonbrew venv use %s' % venv)
 
 def install_scrapy(venv='ENV'):
     run('yum install libffi-devel libxml2-dev libxslt-devel -y')
     with prefix('source "$HOME/.pythonbrew/etc/bashrc"'), prefix('pythonbrew venv use %s' % venv):
         run('pip install lxml')
+        run('pip install service_identity')
         run('pip install Scrapy')
         
+@parallel
 def init():
     print "install git"
     run('yum install git -y')
